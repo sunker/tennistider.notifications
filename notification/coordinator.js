0db9bfa4889b043d894bb7dca8532ff359c7124b
Erik Sundell <erik.sundell87@gmail.com>
2017-10-03 20:51:36 +0200

    Add time frame for notifications

--- notification/coordinator.js
+++ notification/coordinator.js
@@ -13,18 +13,18 @@ module.exports = {
   async notifyUser(slots, email) {
     return new Promise(async(resolve, reject) => {
       var time = new Date().getHours()
-      // if (time >= 7 && time < 23) {
-      let mail = new Mail(slots, email)
-      mailClient.sendEmail(mail).then(() => {
-        console.log(`Mail sent to: ${email}`)
-        resolve()
-      }).catch(err => {
-        console.log(`Could not send mail to: ${email}. Error: ${err}`)
+      if (time >= 5 && time < 21) {
+        let mail = new Mail(slots, email)
+        mailClient.sendEmail(mail).then(() => {
+          console.log(`Mail sent to: ${email}`)
+          resolve()
+        }).catch(err => {
+          console.log(`Could not send mail to: ${email}. Error: ${err}`)
+          reject()
+        })
+      } else {
         reject()
-      })
-      // } else {
-      //   reject()
-      // }
+      }
     })
   },
   async init() {
@@ -32,29 +32,29 @@ module.exports = {
     const users = await User.getAll().then(users => users.filter(x => x.email === 'erik.sundell87@gmail.com' || x.email === 'tobiasolofsson_@hotmail.com' || x.email === 'jonas.bostrom@amendo.se' || x.email === 'gustavsundell@hotmail.com'))
     const slots = await Slot.getAll()
     Promise.all(users.map(user => {
-      return new Promise(async(resolve) => {
-        let slotsOfInterest = userSlotFilter.filterSlots(user, slots)
-        const newSlots = await Promise.all(slotsOfInterest.map(async slot => {
-          const notificationAlreadySent = await Notification.containsKey(slot.getKey(user.email))
-          return notificationAlreadySent ? undefined : {
-            key: slot.getKey(user.email),
-            slot: slot
-          }
-        })).then(result => result.filter(x => x).map(y => y.slot))
+        return new Promise(async(resolve) => {
+          let slotsOfInterest = userSlotFilter.filterSlots(user, slots)
+          const newSlots = await Promise.all(slotsOfInterest.map(async slot => {
+            const notificationAlreadySent = await Notification.containsKey(slot.getKey(user.email))
+            return notificationAlreadySent ? undefined : {
+              key: slot.getKey(user.email),
+              slot: slot
+            }
+          })).then(result => result.filter(x => x).map(y => y.slot))
 
-        if (newSlots.length > 0) {
-          this.notifyUser(newSlots, user.email).then(async() => {
-            const result = await Promise.all(newSlots.map(slot => Notification.add(new Notification({ key: slot.getKey(user.email), timestamp: slot.date }))))
-            console.log(`${result.filter(x => x).length} slots saved`)
+          if (newSlots.length > 0) {
+            this.notifyUser(newSlots, user.email).then(async() => {
+              const result = await Promise.all(newSlots.map(slot => Notification.add(new Notification({ key: slot.getKey(user.email), timestamp: slot.date }))))
+              console.log(`${result.filter(x => x).length} slots saved`)
+              resolve()
+            }, () => {
+              resolve()
+            })
+          } else {
             resolve()
-          }, () => {
-            resolve()
-          })
-        } else {
-          resolve()
-        }
-      })
-    }))
+          }
+        })
+      }))
       .then(() => setTimeout(() => this.init(), process.env.NODE_ENV === 'debug' ? 1000 : 20000))
       .catch(() => setTimeout(() => this.init(), process.env.NODE_ENV === 'debug' ? 1000 : 60000))
   }
