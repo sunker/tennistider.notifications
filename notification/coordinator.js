573f85bf3f2264199f1288bbb9cb1b4eb19133d1
Erik Sundell <erik.sundell87@gmail.com>
2017-12-11 23:21:54 +0100

    Restart on error

--- notification/coordinator.js
+++ notification/coordinator.js
@@ -9,9 +9,14 @@ const mongoose = require('mongoose'),
   mailClient = require('./mailClient'),
   Mail = require('../models/Mail')
 
+const restart = (error) => {
+  console.log(`Force shut down: ${error}`)
+  process.exit(1)
+}
+
 module.exports = {
   async notifyUser(slots, email) {
-    return new Promise(async(resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       var time = new Date().getHours()
       if (time >= 7 && time < 23) {
         let mail = new Mail(slots, email)
@@ -28,35 +33,39 @@ module.exports = {
     })
   },
   async init() {
-    console.log('Time:', new Date())
-    const users = await User.getAll() // .then(users => users.filter(x => x.email === 'erik.sundell87@gmail.com'))
-    const slots = await Slot.getAll()
-    Promise.all(users.map(user => {
-      return new Promise(async(resolve) => {
-        let slotsOfInterest = userSlotFilter.filterSlots(user, slots)
-        let primeTimeSlots = slotsOfInterest.filter(slot => userSlotFilter.isPrimeTime(slot))
-        const newSlots = await Promise.all(primeTimeSlots.map(async slot => {
-          const notificationAlreadySent = await Notification.containsKey(slot.getKey(user.email))
-          return notificationAlreadySent ? undefined : {
-            key: slot.getKey(user.email),
-            slot: slot
-          }
-        })).then(result => result.filter(x => x).map(y => y.slot))
+    try {
+      console.log('Time:', new Date())
+      const users = await User.getAll() // .then(users => users.filter(x => x.email === 'erik.sundell87@gmail.com'))
+      const slots = await Slot.getAll()
+      Promise.all(users.map(user => {
+        return new Promise(async (resolve) => {
+          let slotsOfInterest = userSlotFilter.filterSlots(user, slots)
+          let primeTimeSlots = slotsOfInterest.filter(slot => userSlotFilter.isPrimeTime(slot))
+          const newSlots = await Promise.all(primeTimeSlots.map(async slot => {
+            const notificationAlreadySent = await Notification.containsKey(slot.getKey(user.email))
+            return notificationAlreadySent ? undefined : {
+              key: slot.getKey(user.email),
+              slot: slot
+            }
+          })).then(result => result.filter(x => x).map(y => y.slot))
 
-        if (newSlots.length > 0) {
-          this.notifyUser(newSlots, user.email).then(async() => {
-            const result = await Promise.all(newSlots.map(slot => Notification.add(new Notification({ key: slot.getKey(user.email), timestamp: slot.date }))))
-            console.log(`${result.filter(x => x).length} slots saved`)
+          if (newSlots.length > 0) {
+            this.notifyUser(newSlots, user.email).then(async () => {
+              const result = await Promise.all(newSlots.map(slot => Notification.add(new Notification({ key: slot.getKey(user.email), timestamp: slot.date }))))
+              console.log(`${result.filter(x => x).length} slots saved`)
+              resolve()
+            }, () => {
+              resolve()
+            })
+          } else {
             resolve()
-          }, () => {
-            resolve()
-          })
-        } else {
-          resolve()
-        }
-      })
-    }, () => setTimeout(() => this.init(), process.env.NODE_ENV === 'debug' ? 1000 : 20000)))
-      .then(() => setTimeout(() => this.init(), process.env.NODE_ENV === 'debug' ? 1000 : 20000))
-      .catch(() => setTimeout(() => this.init(), process.env.NODE_ENV === 'debug' ? 1000 : 60000))
+          }
+        })
+      }, error => restart(error)))
+        .then(() => setTimeout(() => this.init(), process.env.NODE_ENV === 'debug' ? 1000 : 20000))
+        .catch(error => restart(error))
+    } catch (error) {
+      restart(error)
+    }
   }
 }
