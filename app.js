0d75f044f69a99e06a9892907d946f4c4bddeb21
Erik Sundell <erik.sundell87@gmail.com>
2017-12-15 07:58:24 +0100

    Restart more frequently

--- app.js
+++ app.js
@@ -30,8 +30,4 @@ app.use(router.routes())
 app.use(router.allowedMethods())
 app.listen(process.env.PORT || '3013')
 
-process.on('uncaughtException', () => process.exit(1))
-
-setTimeout(() => {
-  process.exit(1)
-}, 1000 * 60 * 30)
+setTimeout(() => process.exit(1), 1000 * 60 * 5)
